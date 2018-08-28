1586d38af490599c08921e7648609606e92f9960
tacyarg <jdent@tacyarg.com>
2018-08-28 17:30:37 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/table/lib/css/table.css';
 
+import Navbar from './components/Navbar/Navbar'
 import Trade from './pages/Trade/Trade'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Trade />
       </div>
     );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import {
+  Alignment,
+  Navbar,
+  Tabs
+} from "@blueprintjs/core"
+
+import './Navbar'
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedTabId: 'trade'
+    }
+  }
+
+  render() {
+    return (
+      <Navbar
+        // fixedToTop="true"
+      >
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>
+            <b> Skins.today </b>
+          </Navbar.Heading>
+          <Navbar.Divider />
+
+          <Tabs onChange={this.handleTabChange} selectedTabId="trade">
+            <Tabs.Tab id="trade" title="Trade" />
+            {/* <Tab id="mb" title="Ember" panel={<EmberPanel />} />
+              <Tab id="rx" title="React" panel={<ReactPanel />} />
+              <Tab id="bb" disabled title="Backbone" panel={<BackbonePanel />} />
+              <Tabs.Expander />
+              <input className="bp3-input" type="text" placeholder="Search..." /> */}
+          </Tabs>
+
+        </Navbar.Group>
+      </Navbar>
+    )
+  }
+}
+
+export default Header;
